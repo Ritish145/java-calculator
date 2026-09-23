8dd47678b2a84fcf661c5db017dc413b060a4ecb
Ritish145 <87793001+Ritish145@users.noreply.github.com>
2026-09-23 13:55:50 -0400

    Update CalculationHistoryRepository.java

--- src/main/java/com/calculator/CalculationHistoryRepository.java
+++ src/main/java/com/calculator/CalculationHistoryRepository.java
@@ -1,1 +1,13 @@
+package com.calculator;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CalculationHistoryRepository
+        extends JpaRepository<CalculationHistory, Long> {
+
+    List<CalculationHistory>
+    findTop50ByOrderByCreatedAtDesc();
+
+}
